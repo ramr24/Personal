782f2c63c9f41a41db64a7dadd1d74d94a8c53a9
Ram Rajanbabu <ramr@alleninstitute.org>
2020-02-06 16:19:07 -0800

    update

--- analysis_scripts/read_jem.py
+++ analysis_scripts/read_jem.py
@@ -1,12 +1,16 @@
 """
-Read jem _metadata.csv
+Created on 2/06/2020
+By: Ramkumar Rajanbabu
+
+Purpose:
+-Read csvs
 """
 
 import pandas as pd
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 def read_jem_csv(fields=None):
     """
@@ -27,29 +31,56 @@ def read_jem_csv(fields=None):
             "status", "roi_major", "roi_minor",
             "extraction.postPatch", "extraction.endPipetteR"]
     
-    
-    
     jem = pd.read_csv(csv_path + csv_file, usecols=fields, index_col=["date"])
-    logger.info("Read jem metadata csv (date: 2/04/2020)")
+    LOGGER.info("Read jem metadata csv (date: 2/04/2020)")
+    
     return jem
 
-def sort_df(df):
-    """"""
-    df_2020 = df.loc["2020-01-03 10:40:30 -0800":,:]
-    logger.info("Date Range: 2020-01-03 to Present 2020")
-    
+def sort_df(df, r_users=None, m_users=None):
+    """
+    Args:
+        df:
+        r_users:
+        m_users:
+            
+    Returns:
+        df_2020:
+    """
     r_users = ["kristenh", "lindsayn", "ramr", "katherineb", "jessicat"] 
     m_users = ["P1", "P8", "PA", "PE", "PF"]
     #c_users = ["PC"]
     
+    df_2020 = df.loc["2020-01-03 10:40:30 -0800":,:]
+    LOGGER.info("Date Range: 2020-01-03 to Present 2020")
+    
+    
     df_2020 = df_2020[df_2020.index.notnull()]
-    logger.info("Dropped NaNs from index date column")
+    LOGGER.info("Dropped NaNs from index date column")
+    
     df_2020.dropna(subset=["rigOperator", "container"], inplace=True)
-    logger.info("Dropped NaNs from rigOperator and container column")
+    LOGGER.info("Dropped NaNs from rigOperator and container column")
     
     df_2020 = df_2020[df_2020["status"] == "SUCCESS"]
     df_2020 = df_2020[df_2020.rigOperator.str.contains("|".join(r_users))]
     df_2020 = df_2020[df_2020.container.str.contains("|".join(m_users))]
-    logger.info("Created dataframe with selected users in container column")
+    LOGGER.info("Created dataframe with selected users in container column")
+    
     return df_2020
+
+
+def create_cond_df(df, col_name, cond):
+    """"""
+    df1 = df[df[col_name] == cond]
+    LOGGER.info("Created a conditional dataframe based on jem_2020")
+    return df1
+
+def list_counts():
+    print()
+
+
+if __name__ == "__main__":
+    JEM = read_jem_csv()
+    JEM_2020 = sort_df(JEM)
+    MJEM_2020 = create_cond_df(JEM_2020, "organism_name", "Mouse")
+    HJEM_2020 = create_cond_df(JEM_2020, "organism_name", "Human")
         
