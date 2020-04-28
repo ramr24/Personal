a409242176f87e9015ac9da33cf942052735953b
ramr24 <kumar.rajanbabu@gmail.com>
2020-04-28 15:44:09 -0700

    update for course

--- uw_python_certificate/course_3/lesson_01/flask_calculator/flask-calculator/main.py
+++ uw_python_certificate/course_3/lesson_01/flask_calculator/flask-calculator/main.py
@@ -4,10 +4,20 @@ import os
 
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+app.secret_key = b'\x8a\x90O\x8e\rH\x02\xf9Nb\xcf\xc8\xf6\xe5\x8f\xdf\xb80\x83~\xbf\xdbAs'
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template("add.html")
+    # session['total']
+    if 'total' not in session:
+        session['total'] = 0
+    
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        session['total'] += number
+    
+    
+    return render_template("add.html", session=session)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
